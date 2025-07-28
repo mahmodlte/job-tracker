d40969eae1f60c9362a9ed50a8f833bc35b707e9
Mahmoud Mansour <mahmodlte@gmail.com>
2025-07-28 14:16:55 +0300

    Implemented a custom select field component for styling and resuablity

---
+++ src/components/SelectField.jsx
@@ -0,0 +1,19 @@
+const SelectField = ({ name, label, options, register, required }) => {
+  return (
+    <div className="mb-5">
+      <label className="font-medium block mb-2">{label}</label>
+      <select
+        className="w-full  px-4 py-2 border-gray-400 border-2 rounded-sm mx-3 my-2"
+        id="name"
+        {...register(name, { required })}
+      >
+        <option value="">Select</option>
+        {options.map((option) => {
+          return <option value={option}>{option}</option>;
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default SelectField;
