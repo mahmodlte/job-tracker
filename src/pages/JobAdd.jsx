d853091845a7256ccc492a08d52ef682eef3338f
Mahmoud Mansour <mahmodlte@gmail.com>
2025-07-28 10:47:37 +0300

    A container page for job adding form

---
+++ src/pages/JobAdd.jsx
@@ -0,0 +1,11 @@
+import JobForm from "../componets/JobForm";
+
+const JobAdd = () => {
+  return (
+    <div>
+      <h1>Add a new job to track</h1>
+      <JobForm />
+    </div>
+  );
+};
+export default JobAdd;
