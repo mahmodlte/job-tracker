29a4c8026ba9535b8109ea7718c53367bc3626e2
Mahmoud Mansour <mahmodlte@gmail.com>
2025-07-28 10:44:30 +0300

    Changed from BroswerRouter to RouterProvider
    This is the new method to handle routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-
+import router from "./router.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <RouterProvider router={router}>
       <App />
-    </BrowserRouter>
+    </RouterProvider>
   </StrictMode>
 );
